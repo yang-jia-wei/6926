674b0fac318ea15265d6de8c3d29687e283544c9
yangjiawei <m17635721765@163.com>
2019-09-04 17:09:12 +0800

    [mod]foot.php

--- Index/Tpl/public/foot.php
+++ Index/Tpl/public/foot.php
@@ -1,54 +1,23 @@
-<!--<div id="footer" alog-group="log-footer">-->
-<!--    --><?php //echo $site['inscribe'];?>
-<!--    -->
-<!--</div>-->
-<!--<script type="text/javascript" src="js/superfish.js"></script>-->
-<!---->
-<!--<script>-->
-<!--    (function(){-->
-<!--        var bp = document.createElement('script');-->
-<!--        var curProtocol = window.location.protocol.split(':')[0];-->
-<!--        if (curProtocol === 'https') {-->
-<!--            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';-->
-<!--        }-->
-<!--        else {-->
-<!--            bp.src = 'http://push.zhanzhang.baidu.com/push.js';-->
-<!--        }-->
-<!--        var s = document.getElementsByTagName("script")[0];-->
-<!--        s.parentNode.insertBefore(bp, s);-->
-<!--    })();-->
-<!--</script>-->
-<!---->
-<!--<script>-->
-<!--    (function(){-->
-<!--        var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?f0071a47c222dcf17d16a46760b0c2b2":"https://jspassport.ssl.qhimg.com/11.0.1.js?f0071a47c222dcf17d16a46760b0c2b2";-->
-<!--        document.write('<script src="' + src + '" id="sozz"><\/script>');-->
-<!--    })();-->
-<!--</script><script src="js/ab77b6ea7f3fbf79.js"></script>-->
-<!---->
-<!--<div id="sidr-main" class="sidr left"><div class="sidr-inner"><a href="http://www.puawm.com/#sidr-close" class="sidr-class-toggle-sidr-close" data-wpel-link="internal">X</a>-->
-<!--    </div><div class="sidr-inner">-->
-<!--        <a href="#sidr-main" id="sidr-id-navigation-toggle" class="sidr-class-bars" data-wpel-link="internal"><i class="sidr-class-_mi sidr-class-_before sidr-class-dashicons sidr-class-dashicons-menu"></i></a>-->
-<!---->
-<!--    </div></div></body></html>-->
-<!---->
-
 <div id="footer" alog-group="log-footer">
     <div class="foot">
 
-<!--        <div id="links">-->
-<!--            <ul class="linkcat">-->
-<!--                <li>业务合作、广告租赁请联系：QQ6603183（限正规情感机构）</li>-->
-<!--            </ul>-->
-<!--            <div class="clear"></div>-->
-<!--        </div>-->
+                <div id="links">
+                    <ul class="linkcat">
+                        <li>业务合作、广告租赁请联系：QQ6603183（限正规情感机构）</li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
         <div class="ps">
             <div class="p p2">
-                <?php echo $site['inscribe'];?>
-                <div class="clear"></div>
-                <div class="site-info">
+<!--                --><?php //echo $site['inscribe'];?>
+                <div class="p-content">
                     <?php echo $site['inscribe'];?>
+<!--                    <p class="t2">站点相关</p>-->
+<!--                    <ul>-->
+<!--                        <li><span class="post_spliter">•</span><a href="http://www.puawm.com/1741" target="_blank" data-wpel-link="internal">课程中心</a></li><li><span class="post_spliter">•</span><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=6603183&amp;site=qq&amp;menu=yes" rel="nofollow external noopener noreferrer" target="_blank" data-wpel-link="external">官方客服</a></li><li><span class="post_spliter"></span></li><li><span class="post_spliter">•</span><a href="http://www.puawm.com/sitemap.xml" target="_blank" data-wpel-link="internal">网站地图</a></li></ul>-->
                 </div>
+                <div class="clear"></div>
+                <div class="site-info">Copyright ©  靓男学院 | 闽ICP备14014578号-1</div>
             </div>
             <!-- 若要删除版权请自觉赞助懿古今(支付宝：yigujin@qq.com)20元，谢谢支持 -->
             <div class="p p3">
@@ -71,3 +40,33 @@
         </div>
     </div>
 </div>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script>
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+</script>
+
+<script>
+    (function(){
+        var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?f0071a47c222dcf17d16a46760b0c2b2":"https://jspassport.ssl.qhimg.com/11.0.1.js?f0071a47c222dcf17d16a46760b0c2b2";
+        document.write('<script src="' + src + '" id="sozz"><\/script>');
+    })();
+</script><script src="js/ab77b6ea7f3fbf79.js"></script>
+
+<div id="sidr-main" class="sidr left"><div class="sidr-inner"><a href="http://www.puawm.com/#sidr-close" class="sidr-class-toggle-sidr-close" data-wpel-link="internal">X</a>
+    </div><div class="sidr-inner">
+        <a href="#sidr-main" id="sidr-id-navigation-toggle" class="sidr-class-bars" data-wpel-link="internal"><i class="sidr-class-_mi sidr-class-_before sidr-class-dashicons sidr-class-dashicons-menu"></i></a>
+
+    </div></div></body></html>
+
